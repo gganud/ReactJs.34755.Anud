dce0cd1a92ef73f658b3bec10592c169e161adca
Gaston Anud <gastonanud@gmail.com>
2022-09-17 18:53:53 -0300

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+    return (
+        <div className='divGeneral'>
+            <h1>Hola</h1>
+        </div>
+    );
+}
+
+export default App;
